1dc866972c260a0963d6390c5745b80d0f0b5fba
jestemAria <radmehr.krash@gmail.com>
2022-02-10 16:15:32 +0330

    Text processign before query requests added.

---
+++ text_processing.py
@@ -0,0 +1,48 @@
+from parsivar import SpellCheck
+
+
+def correct_spelling(text):
+    spell_checker = SpellCheck()
+    corrected_text = spell_checker.spell_corrector(text)
+    if corrected_text != text:
+        print("آیا منظور شما این بود؟ ")
+        print(corrected_text)
+        return clean_stop_words_in_query(corrected_text)
+    else:
+        return clean_stop_words_in_query(text)
+
+
+def clean_stop_words_in_query(text):
+    stopword_list = []
+
+    with open("stop_words.txt", encoding="utf-8") as stop_file:
+        stopword_lines = stop_file.readlines()
+        for words in stopword_lines:
+            for word in words.split(","):
+                word = word[:-1]
+                word = word[1:]
+                stopword_list.append(word)
+                # print(word)
+            # print(stopword_list)
+
+        text = text.split()
+        # print(text)
+        for word in text:
+            if word in stopword_list:
+                text.remove(word)
+        cleaned_text = " ".join(text)
+
+        cleaned_text = substitute_persian_with_arabic(cleaned_text)
+        print("LOG ->", "پاکسازی نهایی:", cleaned_text)
+        return cleaned_text
+
+
+def substitute_persian_with_arabic(cleaned_text):
+    for letter in cleaned_text:
+        if letter == "ک":
+            cleaned_text = cleaned_text.replace(letter, "ك")
+        if letter == "ی":
+            cleaned_text = cleaned_text.replace(letter, "ي")
+    return cleaned_text
+
+# correct_spelling("علی از  خاانه در تاریکی با ۲۲ کیلومتر صرعت بیرون می آید")
